f10671530ccb57bda6ea8ee9635d77fd011057a0
hotire <tire.ho@kakaocorp.com>
2019-10-19 20:45:18 +0900

    Add getSupportedSourceVersion latestSupported

--- src/main/java/com/kakao/hotire/java11/apt/HelloProcessor.java
+++ src/main/java/com/kakao/hotire/java11/apt/HelloProcessor.java
@@ -3,9 +3,20 @@ package com.kakao.hotire.java11.apt;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
 public class HelloProcessor extends AbstractProcessor {
+
+  @Override
+  public Set<String> getSupportedAnnotationTypes() {
+    return Set.of(Hello.class.getName());
+  }
+
+  @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.latestSupported();
+  }
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
