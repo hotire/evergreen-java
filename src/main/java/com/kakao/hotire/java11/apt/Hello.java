d01598f53ccc7d99efce3f5f72c7cad6ffdf7d47
hotire <tire.ho@kakaocorp.com>
2019-10-19 20:42:44 +0900

    Add AnnotationProcessor for study

---
+++ src/main/java/com/kakao/hotire/java11/apt/Hello.java
@@ -0,0 +1,11 @@
+package com.kakao.hotire.java11.apt;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Hello {
+}
