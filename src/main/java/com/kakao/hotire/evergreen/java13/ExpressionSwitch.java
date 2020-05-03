d99a31cc2276a2d98ca97b18eb9a50977efd487a
hotire <gngh0101@gmail.com>
2020-05-03 22:53:57 +0900

    Lean how to use switch

---
+++ src/main/java/com/kakao/hotire/evergreen/java13/ExpressionSwitch.java
@@ -0,0 +1,38 @@
+package com.kakao.hotire.evergreen.java13;
+
+public class ExpressionSwitch {
+    int jdkClassic(int input) {
+        int result = 0;
+        switch(input){
+            case 1:
+                result = 1;
+                break;
+            case 2:
+                result = 2;
+                break;
+            default:
+                result = 3;
+                break;;
+        }
+        return result;
+    }
+
+    int jdk12(int input) {
+        return switch (input) {
+            case 1, 2 -> 1;
+            case 3, 4 -> 2;
+            default -> 3;
+        };
+    }
+
+    int jdk13(int input) {
+        return switch (input) {
+            case 1, 2:
+                yield 1;
+            case 3, 4:
+                yield 2;
+            default:
+                yield 3;
+        };
+    }
+}
