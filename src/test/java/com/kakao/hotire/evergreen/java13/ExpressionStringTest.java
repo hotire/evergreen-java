69c124a6a9b5ac4927edd476dbca18ce5776b96a
hotire <gngh0101@gmail.com>
2020-05-04 23:25:18 +0900

    Verify Text Blocks

---
+++ src/test/java/com/kakao/hotire/evergreen/java13/ExpressionStringTest.java
@@ -0,0 +1,24 @@
+package com.kakao.hotire.evergreen.java13;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExpressionStringTest {
+
+    @Test
+    void json() throws JsonProcessingException {
+        // given
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final ExpressionString expressionString = new ExpressionString();
+
+        // when
+        final ExpressionString.User jdk8 = objectMapper.readValue(expressionString.getJdk8(), ExpressionString.User.class);
+        final ExpressionString.User jdk13 = objectMapper.readValue(expressionString.getJdk13(), ExpressionString.User.class);
+
+        // then
+        assertThat(jdk8).isEqualTo(jdk13);
+    }
+}
