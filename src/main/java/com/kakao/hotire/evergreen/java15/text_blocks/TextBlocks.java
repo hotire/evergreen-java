9685843e12aff5c3f177233e126f896d12f4228e
hotire <gngh0101@gmail.com>
2021-05-09 00:09:12 +0900

    Add text-blocks standard in jdk15

---
+++ src/main/java/com/kakao/hotire/evergreen/java15/text_blocks/TextBlocks.java
@@ -0,0 +1,15 @@
+package com.kakao.hotire.evergreen.java15.text_blocks;
+
+/**
+ * standard
+ * https://openjdk.java.net/projects/jdk/15/
+ */
+public class TextBlocks {
+    final String html = """
+              <html>
+                  <body>
+                      <p>Hello, world</p>
+                  </body>
+              </html>
+              """;
+}
