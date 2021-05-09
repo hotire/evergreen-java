3c39d855821a964e726695ba3af597ab575e5043
hotire <gngh0101@gmail.com>
2021-05-09 14:30:42 +0900

    Add comment

--- src/main/java/com/kakao/hotire/evergreen/java15/record/Person.java
+++ src/main/java/com/kakao/hotire/evergreen/java15/record/Person.java
@@ -1,4 +1,7 @@
 package com.kakao.hotire.evergreen.java15.record;
 
+/**
+ * Second Preview
+ */
 public record Person(String name, Integer age) {
 }
--- src/main/java/com/kakao/hotire/evergreen/java15/sealed/Account.java
+++ src/main/java/com/kakao/hotire/evergreen/java15/sealed/Account.java
@@ -1,5 +1,6 @@
 package com.kakao.hotire.evergreen.java15.sealed;
 
+// preview
 // sealed class must has subclasses
 public abstract sealed class Account permits Employee, Manager {
 }
