16b0e3b3c9a55cb6ead09cbed3e024d82641c5fd
hotire <gngh0101@gmail.com>
2021-05-12 00:12:32 +0900

    Use log instead of sout

--- src/main/java/com/kakao/hotire/evergreen/java16/PatternMatchingForInstanceof.java
+++ src/main/java/com/kakao/hotire/evergreen/java16/PatternMatchingForInstanceof.java
@@ -1,4 +1,19 @@
 package com.kakao.hotire.evergreen.java16;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PatternMatchingForInstanceof {
+    void legacy(final Object obj) {
+        if (obj instanceof String) {
+            String s = ((String) obj).toUpperCase();
+            log.info(s);
+        }
+    }
+
+    void jdk16(final Object obj) {
+        if (obj instanceof String s) {
+            log.info(s.toUpperCase());
+        }
+    }
 }
